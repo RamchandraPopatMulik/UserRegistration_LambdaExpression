60879cb7366f69c0d4d06c35c61b79a352e46638
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-12-04 16:46:51 +0530

    Added File

---
+++ User_RegistrationProblem_Using_LambdaExpression/ReflectionOnUserRegistration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_RegistrationProblem_Using_LambdaExpression
+{
+    public class ReflectionOnUserRegistration
+    {
+        public static void ReflectionTest()
+        {
+            Type type = Type.GetType("User_RegistrationProblem_Using_LambdaExpression.Program");
+            Console.WriteLine($"Full Name is : {0}",type.FullName);
+            Console.WriteLine($"Class Name is : {0}",type.Name);
+            MethodInfo[] methods = type.GetMethods();
+            foreach(MethodInfo method in methods)
+            {
+                Console.WriteLine(method.ReturnType.Name+" "+method.Name);
+            }
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine(property.PropertyType.Name + " " + property.Name);
+            }
+            ConstructorInfo[] constructors = type.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine(constructor.ToString());
+            }
+        }
+    }
+}
